ce24505b1a0e0ed9c592c0112804f7c95c647677
Felizardo <devfelizardo@gmail.com>
2020-12-14 02:05:23 -0300

    adding navigation between pages and balance cards totals with formatting

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,8 @@
+// Format the values to show Canadian Dollars as currency
 const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+  Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'CAD',
+  }).format(value);
 
 export default formatValue;
